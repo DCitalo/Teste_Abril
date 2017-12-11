2857ed45b5760d1e0199f79598af6f5d108c1056
DCitalo <cd_italo@hotmail.com>
2017-12-11 17:15:48 -0200

    Layout 1.0.2

--- src/javascripts/default.js
+++ src/javascripts/default.js
@@ -8,3 +8,26 @@ $(function(){
 		} 
 	});  
 });
+
+$('#MBmenu').click(function() {
+	$('#menu').toggleClass('tb-hide');
+	$('#MBmenu .fa-times').toggleClass('hide');
+	$('.fa-bars').toggleClass('hide');
+	$(this).toggleClass('Bg-change');
+
+	$('#search').addClass('tb-hide');
+	$('#MBsearch .fa-times').addClass('hide');
+	$('#MBsearch .fa-search').removeClass('hide');
+	$('#MBsearch').removeClass('Bg-change');
+});
+$('#MBsearch').click(function() {
+	$('#menu').addClass('tb-hide');
+	$('#MBmenu .fa-times').addClass('hide');
+	$('.fa-bars').removeClass('hide');
+	$('#MBmenu').removeClass('Bg-change');
+
+	$('#search').toggleClass('tb-hide');
+	$('#MBsearch .fa-times').toggleClass('hide');
+	$('#MBsearch .fa-search').toggleClass('hide');
+	$(this).toggleClass('Bg-change');
+});
